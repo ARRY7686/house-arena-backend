a557ccd091184cc738c5f60e9578122faef3f45c
sailingsam <jainsaksham1004@gmail.com>
2024-07-07 20:14:57 +0530

    setup mongodb

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 // server.js
 const express = require('express');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
 
-dotenv.config();
+// dotenv.config();
 
-// connectDB();
+// // connectDB();
+
+// const app = express();
+
+// app.use(express.json());
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port http://localhost:${PORT}`);
+// });
+// app.get('/', (req, res) => {
+//     res.send('API is running...');
+// });
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log('MongoDB Connected');
+}).catch((err) => {
+  console.log(err);
+})
 
 const app = express();
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}`);
-});
-app.get('/', (req, res) => {
-    res.send('API is running...');
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
 });
