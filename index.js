2cebf3268a9a0a2922ca8b37a0f16102862a901f
sailingsam <jainsaksham1004@gmail.com>
2024-07-07 14:25:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// server.js
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
